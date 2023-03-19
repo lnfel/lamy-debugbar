8a8712ce9f3052859fc21db2218851b47989f255
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-03-19 16:31:49 +0800

    Prepare app for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+import adapter from '@sveltejs/adapter-static'
+
+const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +10,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+        paths: {
+            base: dev ? '' : '/lamy-debugbar',
+        },
 	},
     preprocess: vitePreprocess()
 };
