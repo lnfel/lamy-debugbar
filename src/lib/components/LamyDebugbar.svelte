6b380dfed6c300cd6f27d48c0841ae46189f3599
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-03-20 15:43:31 +0800

    [Bugfix] Add css manually to LamyDebugbar component, fixes unstyled component when installed via npm

--- src/lib/components/LamyDebugbar.svelte
+++ src/lib/components/LamyDebugbar.svelte
@@ -50,7 +50,7 @@
             bg: shiki?.highlighter.getBackgroundColor(highlighter?.theme ?? 'material-palenight'),
             elements: {
                 pre({ className, style, children }) {
-                    return `<pre class="${className}overflow-x-scroll px-4 py-4" style="${style}" tabindex="0">${children}</pre>`
+                    return `<pre class="${className} overflow-x-scroll px-4 py-4" style="${style}" tabindex="0">${children}</pre>`
                 }
             }
         })
@@ -98,8 +98,8 @@
     }
 </script>
 
-<div class="fixed bottom-0 inset-x-0">
-    <details class="lamy-details relative overflow-hidden {ready ? 'opacity-100' : 'opacity-0'}" bind:open>
+<div class="lamy-debugbar fixed bottom-0 inset-x-0">
+    <details class="lamy-details relative overflow-hidden" style="{ready ? 'display: block;' : 'display: none;'}" bind:open>
         <div class="max-h-96 border-t border-gray-700 overflow-x-hidden overflow-y-auto">
             <!--
                 If block is required so when shiki is available,
@@ -112,7 +112,7 @@
                     {:then shikiHTML} 
                         <div class="lamy-tab-content {key} {i === 0 ? '' : 'hidden'}">{@html shikiHTML}</div>
                         <!-- Show details element when shiki is done rendering all data -->
-                        {#if last(Object.entries(data), i)} <div class="hidden">{lamyIsReady()}</div> {/if}
+                        {#if last(Object.entries(data), i)} <div style="display: none;">{lamyIsReady()}</div> {/if}
                     {/await}
                 {/each}
             {/if}
@@ -156,5 +156,255 @@
 <style>
     summary.lamy-toggle:focus > button.lamy-toggle-arrow {
         background-color: rgb(75 85 99);
+    }
+
+    *,
+    ::before,
+    ::after {
+        box-sizing: border-box;
+        border-width: 0;
+        border-style: solid;
+        border-color: #e5e7eb;
+    }
+
+    ::before,
+    ::after {
+        --tw-content: '';
+    }
+
+    :global(.lamy-debugbar) {
+        line-height: 1.5;
+        -webkit-text-size-adjust: 100%;
+        -moz-tab-size: 4;
+        -o-tab-size: 4;
+            tab-size: 4;
+        font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        font-feature-settings: normal;
+    }
+
+    :global(.lamy-details code),
+    :global(.lamy-details kbd),
+    :global(.lamy-details samp),
+    :global(.lamy-details pre) {
+        font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+        font-size: 1em;
+    }
+
+    :global(.lamy-debugbar button) {
+        font-family: inherit;
+        font-size: 100%;
+        font-weight: inherit;
+        line-height: inherit;
+        color: inherit;
+        margin: 0;
+        padding: 0;
+    }
+
+    :global(.lamy-debugbar button) {
+        text-transform: none;
+    }
+
+    :global(.lamy-debugbar button),
+    :global(.lamy-debugbar [type='button']),
+    :global(.lamy-debugbar [type='reset']),
+    :global(.lamy-debugbar [type='submit']) {
+        -webkit-appearance: button;
+        appearance: button;
+        background-color: transparent;
+        background-image: none;
+    }
+
+    :-moz-focusring {
+        outline: auto;
+    }
+
+    :-moz-ui-invalid {
+        box-shadow: none;
+    }
+
+    :global(.lamy-debugbar summary) {
+        display: list-item;
+    }
+
+    :global(.lamy-debugbar pre) {
+        margin: 0;
+    }
+
+    :global(.lamy-debugbar button),
+    :global(.lamy-debugbar  [role="button"]) {
+        cursor: pointer;
+    }
+
+    :disabled {
+        cursor: default;
+    }
+
+    img,
+    svg {
+        display: block;
+        /* vertical-align: middle; */
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+
+    .sr-only {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        margin: -1px;
+        overflow: hidden;
+        clip: rect(0, 0, 0, 0);
+        white-space: nowrap;
+        border-width: 0;
+    }
+
+    .fixed {
+        position: fixed;
+    }
+
+    .relative {
+        position: relative;
+    }
+
+    .inset-x-0 {
+        left: 0px;
+        right: 0px;
+    }
+
+    .bottom-0 {
+        bottom: 0px;
+    }
+
+    .block {
+        display: block;
+    }
+
+    .flex {
+        display: flex;
+    }
+
+    .hidden {
+        display: none;
+    }
+
+    .h-6 {
+        height: 1.5rem;
+    }
+
+    .max-h-10 {
+        max-height: 2.5rem;
+    }
+
+    .max-h-96 {
+        max-height: 24rem;
+    }
+
+    .w-6 {
+        width: 1.5rem;
+    }
+
+    .min-w-0 {
+        min-width: 0px;
+    }
+
+    .shrink {
+        flex-shrink: 1;
+    }
+
+    .list-none {
+        list-style-type: none;
+    }
+
+    .items-center {
+        align-items: center;
+    }
+
+    .justify-between {
+        justify-content: space-between;
+    }
+
+    .overflow-hidden {
+        overflow: hidden;
+    }
+
+    .overflow-x-auto {
+        overflow-x: auto;
+    }
+
+    .overflow-y-auto {
+        overflow-y: auto;
+    }
+
+    .overflow-x-hidden {
+        overflow-x: hidden;
+    }
+
+    .overflow-x-scroll {
+        overflow-x: scroll;
+    }
+
+    .whitespace-nowrap {
+        white-space: nowrap;
+    }
+
+    .border-t {
+        border-top-width: 1px;
+    }
+
+    .border-gray-700 {
+        --tw-border-opacity: 1;
+        border-color: rgb(55 65 81 / var(--tw-border-opacity))
+    }
+
+    .object-contain {
+        -o-object-fit: contain;
+        object-fit: contain;
+    }
+
+    .px-2 {
+        padding-left: 0.5rem;
+        padding-right: 0.5rem;
+    }
+
+    :global(.shiki.px-4) {
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+
+    .py-2 {
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+    }
+
+    :global(.shiki.py-4) {
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+    }
+
+    .opacity-0 {
+        opacity: 0;
+    }
+
+    .opacity-100 {
+        opacity: 1;
+    }
+
+    .outline-none {
+        outline: 2px solid transparent;
+        outline-offset: 2px;
+    }
+
+    .hover\:bg-gray-600:hover {
+        --tw-bg-opacity: 1;
+        background-color: rgb(75 85 99 / var(--tw-bg-opacity));
+    }
+
+    .focus\:bg-gray-600:focus {
+        --tw-bg-opacity: 1;
+        background-color: rgb(75 85 99 / var(--tw-bg-opacity));
     }
 </style>
