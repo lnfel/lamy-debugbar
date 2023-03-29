ce920598c374a09ba07515580efa32109d13e244
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-03-29 23:04:24 +0800

    [Change] Add types for component and update reexport strategy

---
+++ src/lib/components/LamyDebugbar.svelte.d.ts
@@ -0,0 +1,27 @@
+/** @typedef {typeof __propDef.props}  LamyDebugbarProps */
+/** @typedef {typeof __propDef.events}  LamyDebugbarEvents */
+/** @typedef {typeof __propDef.slots}  LamyDebugbarSlots */
+export default class LamyDebugbar extends SvelteComponentTyped<{
+    data?: {} | undefined;
+    open?: boolean | undefined;
+    highlighter?: import("shiki-es").HighlighterOptions | undefined;
+}, {
+    [evt: string]: CustomEvent<any>;
+}, {}> {
+}
+export type LamyDebugbarProps = typeof __propDef.props;
+export type LamyDebugbarEvents = typeof __propDef.events;
+export type LamyDebugbarSlots = typeof __propDef.slots;
+import { SvelteComponentTyped } from "svelte";
+declare const __propDef: {
+    props: {
+        data?: {} | undefined;
+        open?: boolean | undefined;
+        highlighter?: import("shiki-es").HighlighterOptions | undefined;
+    };
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export {};
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,18 @@
 // Reexport your entry components here
-import { SvelteComponentTyped } from "svelte"
+// import { SvelteComponentTyped } from "svelte"
+/**
+ * 1. First export a class that extends SvelteComponentTyped
+ * 2. npm run build, it will generate a dist folder for the package with the corresponding types
+ * 3. Copy /dist/components/LamyDebugbar.svelte.d.ts to /src/lib/components/LamyDebugbar.svelte.d.ts
+ * 4. After copying the types for our component we can now import it safely in this file
+ */
+// export class LamyDebugbar extends SvelteComponentTyped { }
 
-export class LamyDebugbar extends SvelteComponentTyped { }
+/**
+ * 5. Do the thing, import out component and reexport the thing
+ */
+import LamyDebugbar from "./components/LamyDebugbar.svelte"
+export default LamyDebugbar
+
+// This one also works
+// export { default as LamyDebugbar} from "./components/LamyDebugbar.svelte"
