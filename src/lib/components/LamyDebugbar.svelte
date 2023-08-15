c8cf72b70e75f04e6dcb2e6304e5a702501f2065
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-08-15 09:30:13 +0800

    [Bugfix] Added back missing toggle function for button

--- src/lib/components/LamyDebugbar.svelte
+++ src/lib/components/LamyDebugbar.svelte
@@ -193,6 +193,17 @@
         // console.log('tab.activeForeground: ', currentTheme?.colors?.['tab.activeForeground'])
         // console.log('menu.foreground: ', currentTheme?.colors?.['menu.foreground'])
     })
+
+    /**
+     * Toggle details element
+     */
+    function toggleDetails() {
+        /** @type {HTMLDetailsElement|null} */
+        const detailsElement = document.querySelector('.lamy-details')
+        if (detailsElement) {
+            detailsElement.open = !detailsElement.open
+        }
+    }
 </script>
 
 {#if ready}
@@ -233,7 +244,7 @@
                     </div>
                 </div>
 
-                <button class={`lamy-toggle-arrow ${tw(`px-2 py-2 outline-none hover:text-[${currentTheme?.colors?.['tab.activeForeground']}] hover:bg-[${currentTheme?.colors?.['titleBar.inactiveForeground'] ?? currentTheme?.colors?.['titleBar.activeBackground'] ?? currentTheme?.colors?.['menu.background']}]`)}`} type="button">
+                <button on:click={toggleDetails} class={`lamy-toggle-arrow ${tw(`px-2 py-2 outline-none hover:text-[${currentTheme?.colors?.['tab.activeForeground']}] hover:bg-[${currentTheme?.colors?.['titleBar.inactiveForeground'] ?? currentTheme?.colors?.['titleBar.activeBackground'] ?? currentTheme?.colors?.['menu.background']}]`)}`} type="button">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class={`${tw('w-6 h-6')}`}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                     </svg>
